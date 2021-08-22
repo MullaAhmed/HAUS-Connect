7aa7da7f2097dedeeb5411c35c4540ab6a373435
MullaAhmed <67974888+MullaAhmed@users.noreply.github.com>
2021-08-22 18:17:07 +0530

    Update settings.py

--- broadcaster/settings.py
+++ broadcaster/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+"""
 STATIC_URL = '/static/'
 if DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'broadcaster/static').replace('\\', '/')
@@ -137,6 +137,16 @@ else:
         os.path.join(BASE_DIR, 'broadcaster/static').replace('\\', '/')
     ]
     
+"""
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'broadcaster/static'),
+       
+    )
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
